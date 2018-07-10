4d3cfeeb2d1cd5a485e54d495e2da9afec0ab848
Ty Brown <tyriek.brown02@gmail.com>
2018-07-10 09:09:22 -0500

    Initial commit

---
+++ fullstack-auth/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://newdata:Password02@ds125881.mlab.com:25881/tyriek' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
